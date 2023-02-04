a75da093c57daa4fe0e0cf393b1eb515d3921ab5
Genaro <antaresgen@gmail.com>
2023-02-04 05:44:58 -0600

    modified path

--- workspace/week2/flows/etl_web_to_gcs_github.py
+++ workspace/week2/flows/etl_web_to_gcs_github.py
@@ -38,7 +38,7 @@ def write_gcs(path: Path) -> None:
 @task()
 def write_local(df: pd.DataFrame, color: str, dataset_file: str) -> Path:
     """rite Dataframe out locally as parquet file"""
-    path = Path(f"../data/{color}/{dataset_file}.parquet")
+    path = Path(f"../../data/{color}/{dataset_file}.parquet")
     df.to_parquet(path, compression="gzip")
     return path
 
--- workspace/week2/flows/github_storage_deploy.py
+++ workspace/week2/flows/github_storage_deploy.py
@@ -8,7 +8,7 @@ deployment_github_storage = Deployment.build_from_flow(
     flow=etl_web_to_gcs_github,
     name="github-flow",
     storage=github_storage,
-    entrypoint="workspace/week2/etl_web_to_gcs_github.py:etl_web_to_gcs_github",
+    entrypoint="workspace/week2/flows/etl_web_to_gcs_github.py:etl_web_to_gcs_github",
 )
 
 deployment_github_storage.apply()
