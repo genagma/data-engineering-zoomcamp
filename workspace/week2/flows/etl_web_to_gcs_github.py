f46bd95b0069196851f03320741b6c45f9dae41a
Genaro <antaresgen@gmail.com>
2023-02-05 00:07:41 -0600

    month changed to april

--- workspace/week2/flows/etl_web_to_gcs_github.py
+++ workspace/week2/flows/etl_web_to_gcs_github.py
@@ -52,7 +52,7 @@ def etl_web_to_gcs_github() -> None:
     """The main ETL function"""
     color = "green"
     year = 2019
-    month = 3
+    month = 4
     dataset_file = f"{color}_tripdata_{year}-{month:02}"
     dataset_url = f"https://github.com/DataTalksClub/nyc-tlc-data/releases/download/{color}/{dataset_file}.csv.gz"
     
